9d86ac4dea0c3e3cc79cce0f6517eb66d7e29e02
RxGoodness <adedokuntoyyib@gmail.com>
2022-07-20 16:14:25 +0100

    files

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,11 @@ import express from 'express';
 import path from 'path';
 import cookieParser from 'cookie-parser';
 import logger from 'morgan';
-import usersRouter from './routes/users';
 
 const app = express();
+
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
 
 // view engine setup
 app.set('views', path.join(__dirname, '..', 'views'));
@@ -17,6 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
+app.use('/', indexRouter);
 app.use('/', usersRouter);
 
 // catch 404 and forward to error handler
@@ -29,7 +32,7 @@ app.use(function (
   err: createError.HttpError,
   req: express.Request,
   res: express.Response,
-  _next: express.NextFunction,
+  _next: express.NextFunction
 ) {
   // set locals, only providing error in development
   res.locals.message = err.message;
